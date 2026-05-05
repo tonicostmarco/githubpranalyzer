01555d639e0399970e6b6e4b4073664cde31294c
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-05 20:24:00 -0300

    config: add RabbitMQ exchange, queue and binding

--- src/main/java/com/tonicostmarco/githubpranalyzer/config/RabbitMQConfig.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/config/RabbitMQConfig.java
@@ -10,19 +10,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-        // Declara a fila
         @Bean
         public Queue prEventsQueue() {
-            return new Queue("pr-events", true); // true = durable (sobrevive a restart)
+            return new Queue("pr-events", true);
         }
 
-        // Declara o exchange
         @Bean
         public TopicExchange prExchange() {
             return new TopicExchange("pr-exchange");
         }
-
-        // Liga a fila ao exchange com um padrão
         @Bean
         public Binding binding(Queue prEventsQueue, TopicExchange prExchange) {
             return BindingBuilder.bind(prEventsQueue).to(prExchange).with("pr.*");
