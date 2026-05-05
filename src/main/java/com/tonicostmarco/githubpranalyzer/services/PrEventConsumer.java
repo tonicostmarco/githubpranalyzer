cb8a207c198370c52a4827c153c0619b5394766d
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-05 20:24:00 -0300

    feat: add PrEventConsumer with idempotency check

---
+++ src/main/java/com/tonicostmarco/githubpranalyzer/services/PrEventConsumer.java
@@ -0,0 +1,43 @@
+package com.tonicostmarco.githubpranalyzer.services;
+
+import com.tonicostmarco.githubpranalyzer.dtos.PrEventMessage;
+import com.tonicostmarco.githubpranalyzer.dtos.PrWebhookPayload;
+import com.tonicostmarco.githubpranalyzer.entities.PrEvent;
+import com.tonicostmarco.githubpranalyzer.repositories.PrEventRepository;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class PrEventConsumer {
+
+    private final PrEventRepository repository;
+
+    public PrEventConsumer(PrEventRepository repository) {
+        this.repository = repository;
+    }
+
+    @RabbitListener(queues = "pr-events")
+    public void consume(PrEventMessage message) {
+
+        if (repository.existsByDeliveryId(message.deliveryId())) {
+            return;
+        }
+
+        PrEvent event = new PrEvent();
+
+        event.setDeliveryId(message.deliveryId());
+        event.setAction(message.payload().action());
+        event.setPrNumber(message.payload().number());
+        event.setPrTitle(message.payload().pullRequest().title());
+        event.setPrState(message.payload().pullRequest().state());
+        event.setMerged(message.payload().pullRequest().merged());
+        event.setPrAuthor(message.payload().pullRequest().user().login());
+        event.setRepository(message.payload().repository().fullName());
+        event.setReceivedAt(LocalDateTime.now());
+
+        repository.save(event);
+    }
+}
+
