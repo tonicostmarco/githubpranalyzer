9c67fa5bdcc3d46c65a3b121eeda8cfd8b4f64e9
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-11 19:36:47 -0300

    refactor(producer): change send signature to accept PrEventMessage directly

--- src/main/java/com/tonicostmarco/githubpranalyzer/services/PrEventProducer.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/services/PrEventProducer.java
@@ -1,21 +1,31 @@
 package com.tonicostmarco.githubpranalyzer.services;
 
+import com.tonicostmarco.githubpranalyzer.dtos.PrEventMessage;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PrEventProducer {
 
-        private final RabbitTemplate rabbitTemplate;
 
-        public PrEventProducer(RabbitTemplate rabbitTemplate) {
-            this.rabbitTemplate = rabbitTemplate;
-        }
+    private RabbitTemplate template;
 
-        public void send(String action, Object payload) {
-            String routingKey = "pr." + action; // "pr.opened", "pr.closed"...
-            rabbitTemplate.convertAndSend("pr-exchange", routingKey, payload);
-        }
+    public PrEventProducer(RabbitTemplate template) {
+
+        this.template = template;
 
     }
+
+    public void send(PrEventMessage message) {
+
+
+
+        template.convertAndSend("pr-exchange", "pr." + message.payload().action(), message);
+
+    }
+
+}
+
+
+
 
