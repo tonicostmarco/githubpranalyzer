a98e27628ef00da64cac9ebed1338f8f99142064
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-11 19:36:47 -0300

    fix: remove extra whitespace in class declaration

--- src/main/java/com/tonicostmarco/githubpranalyzer/GithubpranalyzerApplication.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/GithubpranalyzerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GithubpranalyzerApplication {
+public class  GithubpranalyzerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubpranalyzerApplication.class, args);
