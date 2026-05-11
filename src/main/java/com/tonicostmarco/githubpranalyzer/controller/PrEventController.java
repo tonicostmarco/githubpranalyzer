2822bdc8dbb86fb67c391175620182f84d952b48
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-11 19:36:47 -0300

    feat(controller): add POST /webhook/notify endpoint to receive GitHub PR webhooks

--- src/main/java/com/tonicostmarco/githubpranalyzer/controller/PrEventController.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/controller/PrEventController.java
@@ -1,4 +1,32 @@
 package com.tonicostmarco.githubpranalyzer.controller;
 
+import com.tonicostmarco.githubpranalyzer.dtos.PrEventMessage;
+import com.tonicostmarco.githubpranalyzer.dtos.PrWebhookPayload;
+import com.tonicostmarco.githubpranalyzer.services.PrEventProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/webhook/notify")
 public class PrEventController {
+
+    private final PrEventProducer producer;
+
+    public PrEventController(PrEventProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping
+
+    public ResponseEntity<PrEventMessage> webhookReceiver(@RequestHeader("X-GitHub-Delivery") String deliveryId, @RequestBody PrWebhookPayload payload) {
+
+
+
+        PrEventMessage eventMessage = new PrEventMessage(deliveryId, payload);
+
+        producer.send(eventMessage);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
