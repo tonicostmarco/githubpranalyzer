a5fe500af16a7f98b0957284bb1dbc3f68c9f248
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-12 20:02:08 -0300

    feat(analytics): add GET /analytics/summary endpoint with date range filter

--- src/main/java/com/tonicostmarco/githubpranalyzer/controller/AnalyticsController.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/controller/AnalyticsController.java
@@ -1,4 +1,30 @@
 package com.tonicostmarco.githubpranalyzer.controller;
 
+import com.tonicostmarco.githubpranalyzer.dtos.analytics.SummaryDTO;
+import com.tonicostmarco.githubpranalyzer.services.AnalyticsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/analytics")
 public class AnalyticsController {
+
+
+    private final AnalyticsService service;
+
+    public AnalyticsController(AnalyticsService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/summary")
+    public ResponseEntity<SummaryDTO> findSummary(@RequestParam(name = "from") LocalDateTime from, @RequestParam(name = "to")LocalDateTime to) {
+
+        SummaryDTO dto = service.findSummary(from, to);
+
+        return ResponseEntity.ok(dto);
+
+    }
+
 }
