a579a06469662679e7fcda36e96400370c5de787
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-13 19:46:20 -0300

    feat(analytics): add findAuthorMetrics aggregation grouped by prAuthor

--- src/main/java/com/tonicostmarco/githubpranalyzer/services/AnalyticsService.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/services/AnalyticsService.java
@@ -1,17 +1,16 @@
 package com.tonicostmarco.githubpranalyzer.services;
 
+import com.tonicostmarco.githubpranalyzer.dtos.analytics.AuthorMetricsDTO;
 import com.tonicostmarco.githubpranalyzer.dtos.analytics.SummaryDTO;
 import com.tonicostmarco.githubpranalyzer.entities.PrEvent;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.ConditionalOperators;
-import org.springframework.data.mongodb.core.aggregation.GroupOperation;
-import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.*;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class AnalyticsService {
@@ -31,14 +30,13 @@ public class AnalyticsService {
         GroupOperation group = Aggregation.group()
 
                 .count().as("total")
-                .sum(ConditionalOperators.when(Criteria.where("merged")
-                        .is(true)).then(1).otherwise(0))
-                .as("merged")
-                .sum(ConditionalOperators.when(Criteria.where("prState")
-                        .is("open")).then(1).otherwise(0))
+                .sum(ConditionalOperators.when(Criteria.where("merged").is(true)).then(1)
+                        .otherwise(0)).as("merged")
+                .sum(ConditionalOperators.when(Criteria.where("prState").is("open")).then(1)
+                        .otherwise(0))
                 .as("opened")
-                .sum(ConditionalOperators.when(Criteria.where("prState")
-                        .is("closed")).then(1).otherwise(0))
+                .sum(ConditionalOperators.when(Criteria.where("prState").is("closed")).then(1)
+                        .otherwise(0))
                 .as("closed");
 
 
@@ -46,6 +44,34 @@ public class AnalyticsService {
 
 
         return template.aggregate(aggregation, "pr_events", SummaryDTO.class).getUniqueMappedResult();
+
+    }
+
+    public List<AuthorMetricsDTO> findAuthorMetrics(LocalDateTime from, LocalDateTime to) {
+
+        //montar stages
+
+        MatchOperation match = Aggregation.match(Criteria.where("receivedAt").gte(from).lte(to));
+
+        GroupOperation group = Aggregation.group("prAuthor")
+                .count().as("total")
+                .sum(ConditionalOperators.when(Criteria.where("merged").is(true)).then(1)
+                        .otherwise(0)).as("merged")
+                .sum(ConditionalOperators.when(Criteria.where("prState").is("open")).then(1)
+                                .otherwise(0)).as("opened")
+                .sum(ConditionalOperators.when(Criteria.where("prState").is("closed")).then(1)
+                                .otherwise(0)).as("closed");
+
+        ProjectionOperation projectionOperation = Aggregation.project().and("_id").as("author")
+                .andInclude("total", "merged", "opened", "closed");
+
+        //juntar na aggregation
+
+        Aggregation aggregation = Aggregation.newAggregation(match, group, projectionOperation);
+
+        //executar a aggregation definindo para qual dto vai ser mapeado
+
+        return template.aggregate(aggregation, "pr_events", AuthorMetricsDTO.class).getMappedResults();
 
     }
 
