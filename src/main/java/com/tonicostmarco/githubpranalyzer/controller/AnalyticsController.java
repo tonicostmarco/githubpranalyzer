d6aa87ccf380a86e150c04c6f9e3f4a6b904c303
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-13 19:46:20 -0300

    feat(analytics): add GET /analytics/authormetrics endpoint

--- src/main/java/com/tonicostmarco/githubpranalyzer/controller/AnalyticsController.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/controller/AnalyticsController.java
@@ -1,11 +1,13 @@
 package com.tonicostmarco.githubpranalyzer.controller;
 
+import com.tonicostmarco.githubpranalyzer.dtos.analytics.AuthorMetricsDTO;
 import com.tonicostmarco.githubpranalyzer.dtos.analytics.SummaryDTO;
 import com.tonicostmarco.githubpranalyzer.services.AnalyticsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/analytics")
@@ -22,6 +24,15 @@ public class AnalyticsController {
     public ResponseEntity<SummaryDTO> findSummary(@RequestParam(name = "from") LocalDateTime from, @RequestParam(name = "to")LocalDateTime to) {
 
         SummaryDTO dto = service.findSummary(from, to);
+
+        return ResponseEntity.ok(dto);
+
+    }
+
+    @GetMapping(value = "/authormetrics")
+    public ResponseEntity<List<AuthorMetricsDTO>> findAuthorMetrics(@RequestParam(name = "from") LocalDateTime from, @RequestParam(name = "to") LocalDateTime to) {
+
+        List<AuthorMetricsDTO> dto = service.findAuthorMetrics(from, to);
 
         return ResponseEntity.ok(dto);
 
