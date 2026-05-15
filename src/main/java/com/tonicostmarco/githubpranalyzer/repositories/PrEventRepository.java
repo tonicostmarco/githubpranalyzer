3a890506a22283adb42fb6f1d27e91f177dfd97d
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-15 20:36:57 -0300

    feat(repo): add findByDeliveryId method to PrEventRepository (not tested)

--- src/main/java/com/tonicostmarco/githubpranalyzer/repositories/PrEventRepository.java
+++ src/main/java/com/tonicostmarco/githubpranalyzer/repositories/PrEventRepository.java
@@ -3,10 +3,12 @@ package com.tonicostmarco.githubpranalyzer.repositories;
 import com.tonicostmarco.githubpranalyzer.entities.PrEvent;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface PrEventRepository extends MongoRepository<PrEvent, String> {
 
     boolean existsByDeliveryId(String s);
 
 
-
+    Optional<PrEvent> findByDeliveryId(String s);
 }
